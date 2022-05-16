39c7598d5bcbe727e772af400206196c46fa58ed
joan kariuki <joan,kariuki@student.moringaschool.com>
2022-05-16 13:01:56 +0300

    first commit

---
+++ src/main/java/CeaserCipher/App.java
@@ -0,0 +1,37 @@
+package CeaserCipher;
+
+import java.util.Scanner;
+
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String password;
+        int key;
+
+        System.out.print("Please enter the sentence: ");
+        password = keyboard.nextLine();
+
+        do {
+            System.out.print("Please enter a key between 1 to 25: ");
+            key = keyboard.nextInt();
+
+            //here we handle key errors
+            if (key < 1 || key > 25) {
+                System.out.printf(" The key must be between 1 and 25, you entered %d.\n", key);
+            }
+        } while (key < 1 || key > 25);
+
+
+        System.out.println("Password:\t" + password);
+        CeaserCipher ceaserCipher = new CeaserCipher(password,key);
+        String encryption = ceaserCipher.cipher();
+        System.out.println("Encrypted:\t" + encryption);
+
+        //decryption
+        CeaserCipher ceaserCipherDec = new CeaserCipher(encryption,-key);
+        System.out.println("Decrypted:\t" + ceaserCipherDec.cipher());
+
+    }
+}
